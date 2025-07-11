d7f462177eaa067e57bf2fd4cb01d4b9ee6924a3
Samuel Lucio <samuel.lucio>
2025-07-11 00:02:50 -0300

    - interface grafo

---
+++ Arvores/Arvores/Estruturas_de_Arvores/src/Grafos/GrafoSimples.java
@@ -0,0 +1,5 @@
+package Grafos;
+
+public class GrafoSimples {
+
+}
--- Arvores/Arvores/Estruturas_de_Arvores/src/Grafos/TadGrafo.java
+++ Arvores/Arvores/Estruturas_de_Arvores/src/Grafos/TadGrafo.java
@@ -1,19 +1,32 @@
 package Grafos;
 
+import java.util.Collection;
 import java.util.List;
 
-public interface TadGrafo {
+public interface TadGrafo<T extends Object> {
 
-    List<Vertice> finalVertices(Aresta aresta);
+    List<Vertice<T>> finalVertices(Aresta aresta);
 
-    Vertice oposto(Vertice vertice, Aresta aresta);
+    Vertice<T> oposto(Vertice<T> vertice, Aresta aresta);
 
-    boolean isAdjacente(Vertice vertice1, Vertice vertice2);
+    boolean isAdjacente(Vertice<T> vertice1, Vertice<T> vertice2);
 
-    void substituir(Vertice verticeSubstituido, Vertice verticeSubstituto);
+    void substituir(Vertice<T> verticeSubstituido, Vertice<T> verticeSubstituto);
 
     void substituir(Aresta arestaSubstituida, Aresta arestaSubstituta);
 
-    void inserirVertice();
+    Vertice<T> inserirVertice(T o);
+
+    Aresta inserirAresta(Vertice<T> verticeInsert, Vertice<T> verticeInicio, Vertice<T> verticeFim);
+
+    T removerVertice(Vertice<T> vertice);
+
+    Aresta removerAresta(Aresta aresta);
+
+    Collection<Aresta> arestasIncidentes(Vertice<T> vertice);
+
+    Collection<Aresta> arestas();
+
+    Collection<Vertice<T>> vertices();
 
 }
