5a35a6a4d0e135e85e00f2655d15930c1c21ed76
Samuel Lucio <samuel.lucio>
2025-07-30 00:16:52 -0300

    goodman

--- Arvores/AlgoritmoGrafos/AlgoritmosCaminho/src/Decisao/Goodman.java
+++ Arvores/AlgoritmoGrafos/AlgoritmosCaminho/src/Decisao/Goodman.java
@@ -1,28 +1,28 @@
 package Decisao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import melhorcaminho.TupleMatriz;
 
 public class Goodman { 
 
-    public boolean isConexo(List<TupleMatriz> listaVertices) {
-        int grau = 0;
-        int sum = 0;
-        int linhaAtual = 0;
-        final var matriz = preencherMatriz(listaVertices);
-
-        while (sum <= 2 && linhaAtual <= matriz.length) {
-            grau = 0;
-            for (int i = 0; i < matriz.length; i++) {
-                grau+=matriz[linhaAtual][i];
+    public boolean isConexo(List<Vertice> listaVertices) {
+        int seq=0;
+        final var grafoResutalte = new HashMap<String, Vertice>();
+        alimentarGrafo(listaVertices, grafoResutalte);
+        while (!listaVertices.isEmpty()) {
+            final var verticeInterado = listaVertices.remove(0);
+            for (Vertice vertice : listaVertices) {
+                if (isAdjacente(verticeInterado, vertice)) {
+                   
+                }
             }
-            if (grau%2 == 1) {
-                sum++;
-            }
-            linhaAtual++;
         }
-        return sum>2;
+        return false;
     }
 
     private int[][] preencherMatriz(List<TupleMatriz> listaVertices) {
@@ -38,7 +38,14 @@ public class Goodman {
     }
 
 
-
+    public void alimentarGrafo(List<Vertice> vertices, Map<String,Vertice> map){
+        int index = 1;
+        for (Vertice vertice : vertices) {
+            final var indexStr = Integer.toString(index);
+            map.put(indexStr, vertice);
+            index++;
+        }
+    }
 
 
 
@@ -64,21 +71,24 @@ public class Goodman {
     }
     
     public class Aresta {
-         private Vertice Vertice1;
-         private Vertice vertice2;
+        private Vertice Vertice1;
+        private Vertice vertice2;
 
-         public Vertice getVertice1() {
-             return Vertice1;
-         }
-         public void setVertice1(Vertice vertice1) {
-             Vertice1 = vertice1;
-         }
-         public Vertice getVertice2() {
-             return vertice2;
-         }
-         public void setVertice2(Vertice vertice2) {
-             this.vertice2 = vertice2;
-         }         
+        public Vertice getVertice1() {
+            return Vertice1;
+        }
+
+        public void setVertice1(Vertice vertice1) {
+            Vertice1 = vertice1;
+        }
+
+        public Vertice getVertice2() {
+            return vertice2;
+        }
+
+        public void setVertice2(Vertice vertice2) {
+            this.vertice2 = vertice2;
+        }
 
     }
 
